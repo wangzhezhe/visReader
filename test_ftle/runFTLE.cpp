5a32d1aa1ba1c0d3706aa5d5d7137d57d041d19d
Jay <godenwangzhe@gmail.com>
2023-12-06 03:47:29 +0000

    update ftle

--- test_ftle/runFTLE.cpp
+++ test_ftle/runFTLE.cpp
@@ -8,31 +8,41 @@
 #include <vtkm/cont/ArrayCopy.h>
 #include <vtkm/cont/DataSetBuilderUniform.h>
 #include <vtkm/filter/flow/LagrangianStructures.h>
-
+#include <vtkm/cont/Initialize.h>
+#include <vtkm/filter/flow/Tracer.h>
 int main(int argc, char *argv[])
 {
-    if (argc != 3)
+    vtkm::cont::Initialize(argc, argv);
+    if (argc != 4)
     {
-        std::cout << "<binary> <file name> <field name>" << std::endl;
+        std::cout << "<binary> <input file name> <field name> <output file name>" << std::endl;
         exit(0);
     }
 
     std::string fileName = argv[1];
     std::string fieldName = argv[2];
+    std::string outputFileName = argv[3];
+
+    vtkm::filter::flow::GetTracer().Get()->Init(0);
+    vtkm::filter::flow::GetTracer().Get()->StartTimer();
 
     vtkm::io::VTKDataSetReader reader(fileName);
     vtkm::cont::DataSet inputData = reader.ReadDataSet();
 
+    std::cout << "input data " << std::endl;
+    inputData.PrintSummary(std::cout);
+
     vtkm::filter::flow::LagrangianStructures lagrangianStructures;
     lagrangianStructures.SetStepSize(0.025f);
-    lagrangianStructures.SetNumberOfSteps(500);
-    lagrangianStructures.SetAdvectionTime(0.025f * 500);
+    lagrangianStructures.SetNumberOfSteps(100);
+    lagrangianStructures.SetAdvectionTime(0.025f * 100);
     lagrangianStructures.SetActiveField(fieldName);
     vtkm::cont::DataSet outputData = lagrangianStructures.Execute(inputData);
+    vtkm::filter::flow::GetTracer().Get()->Finalize();
 
     outputData.PrintSummary(std::cout);
 
-    std::string outputFileName = "output_ftle.vtk";
+    //std::string outputFileName = "output_ftle.vtk";
     vtkm::io::VTKDataSetWriter write(outputFileName);
     write.SetFileTypeToBinary();
     write.WriteDataSet(outputData);
